3922db996fa6573a75763a19281a147b3d49ec39
XinnaPan <996386736@qq.com>
2021-03-06 14:42:02 +0800

    没有解决只有一个数字的情况

---
+++ 1965.cpp
@@ -0,0 +1,111 @@
+//"给定一个包含正整数、加(+)、减(-)、乘(*)、除(/)的算数表达式(括号除外)，计算其结果。
+//
+//表达式仅包含非负整数， + ， - ， * ， / 四种运算符和空格  。 整数除法仅保留整数部分。
+//
+//来源：力扣（LeetCode）
+//链接：https ://leetcode-cn.com/problems/calculator-lcci
+//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。"
+
+#include<iostream>
+#include<string>
+#include<stack>
+using namespace std;
+
+class Solution
+{
+	int doCal(int a, int b, char s)
+	{
+		int res;
+		switch (s)
+		{
+		case '+':
+			return a + b;
+		case '-':
+			return a - b;
+		case '*':
+			return a * b;
+		case '/':
+			return a / b;
+		}
+		return 0;
+	}
+public:
+	int calculate(string s)
+	{
+		stack<char> ss;
+		stack<int> si;
+		for (int i = 0; i < s.length(); i++)
+		{
+			if (s[i] <= '9' && s[i] >= '0')
+			{
+				si.push(s[i] - '0');
+			}
+			else //symbol
+			{
+				if (s[i] == ' ')
+					continue;
+				if (ss.empty())
+				{
+					ss.push(s[i]);
+					continue;
+				}
+				if (s[i] == '*' || s[i] == '/')
+				{
+					if (ss.top() == '+' || ss.top() == '-')
+						ss.push(s[i]);
+					else
+					{
+						int iTemp;
+						int a = si.top();
+						si.pop();
+						int b = si.top();
+						si.pop();
+						iTemp = doCal(b,a, ss.top());
+						ss.pop();
+						si.push(iTemp);
+						i--;
+
+					}
+				}
+				else if (s[i] == '+' || s[i] == '-')
+				{
+					int iTemp;
+					int a = si.top();
+					si.pop();
+					int b = si.top();
+					si.pop();
+					iTemp = doCal(b, a, ss.top());
+					ss.pop();
+					si.push(iTemp);
+					i--;
+				}
+			
+			}
+		}
+
+		while (!ss.empty())
+		{
+			int iTemp;
+			int a = si.top();
+			si.pop();
+			int b = si.top();
+			si.pop();
+			iTemp = doCal(b, a, ss.top());
+			ss.pop();
+			si.push(iTemp);
+
+		}
+
+		int ret = si.top();
+		si.pop();
+		return ret;
+	}
+};
+int main()
+{
+	string s;
+	Solution res;
+	cin >> s;
+	cout << res.calculate(s);
+	return 0;
+}
